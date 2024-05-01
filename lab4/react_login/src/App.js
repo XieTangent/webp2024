822bd52c611a539ae7dd8c52e6aa1dede62513cb
B1129036 <119582568+XieTangent@users.noreply.github.com>
2024-05-01 15:02:56 +0800

    Add files via upload

---
+++ lab4/react_login/src/App.js
@@ -0,0 +1,15 @@
+import multiButton from './cgu_multiButton';
+import HelloCGU from './cgu_HelloCGU'
+import CGU_Login from './cgu_login';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        { CGU_Login() }
+      </div>
+    </div>
+  );
+}
+
+export default App;
